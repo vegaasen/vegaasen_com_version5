6065a209583f72a4ae2cde78b1435c2dc5f160ee
vegaasen <vegaasen@gmail.com>
2014-07-07 17:48:32 +0200

    Moved/Removed files for the very much unstructured site..

---
+++ resources/js/my.js
@@ -0,0 +1,16 @@
+function destroyLessCache(pathToCss) { // e.g. '/css/' or '/stylesheets/'
+ 
+  var host = window.location.host;
+  var protocol = window.location.protocol;
+  var keyPrefix = protocol + '//' + host + pathToCss;
+  
+  for (var key in window.localStorage) {
+    if (key.indexOf(keyPrefix) === 0) {
+      delete window.localStorage[key];
+    }
+  }
+}
+
+window.onload = function() {
+  destroyLessCache("/resources/");
+}
