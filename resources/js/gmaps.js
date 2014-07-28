fbcc848fad7858abb665fa33f7eb597ec797bdf5
vegaasen <vegaasen@gmail.com>
2014-07-28 12:38:03 +0200

    Added a image and a new less profile for texts.

--- resources/js/gmaps.js
+++ resources/js/gmaps.js
@@ -1,45 +1,176 @@
+const MAP_ID = 'vegardAasen';
+
 var mapId = "map-me";
 var map;
-var myWhereabouts = new google.maps.LatLng(40.6743890, -73.9455);
+var myWhereabouts = new google.maps.LatLng(59.9038127,10.7876544);
+var wo = new google.maps.LatLng(59.898766,10.6261);
+var te = new google.maps.LatLng(59.92384,10.731749);
+var sp = new google.maps.LatLng(59.913196,10.732692);
 var mapColor = "#363636";
-
-var MY_MAPTYPE_ID = 'custom_style';
+var marker = "/resources/img/map/location.png";
 
 function initialize() {
-  var featureOpts = [
-    {
-      stylers: [
-        { hue: mapColor },
-        { visibility: 'simplified' },
-        { gamma: 1.5 },
-        { weight: 0.5 }
-      ]
-    },
-    {
-      elementType: 'labels',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    },
-    {
-      featureType: 'water',
-      stylers: [
-        { color: mapColor }
-      ]
-    }
+  var featureOpts = 
+  [
+      {
+          "featureType": "administrative",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.country",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "on"
+              },
+              {
+                  "color": "#f5f5f5"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape.natural",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#cccccc"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.airport",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.airport",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "color": "#13d0a8"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      }
   ];
   var mapOptions = {
-    zoom: 12,
+    zoom: 11,
     center: myWhereabouts,
     mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MAP_ID]
     },
-    mapTypeId: MY_MAPTYPE_ID
+    mapTypeId: MAP_ID,
+    draggable: false,
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true
   };
 
   map = new google.maps.Map(document.getElementById(mapId), mapOptions);
-  var styledMapOptions = {name: 'Custom Style'};
+  var styledMapOptions = {name: 'VegardsWhereabouts'};
   var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
-  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+  new google.maps.Marker({position: myWhereabouts,map: map,icon: marker,opacity:0.9});
+  new google.maps.Marker({position: wo,map: map,icon: marker,opacity:0.8});
+  new google.maps.Marker({position: te,map: map,icon: marker,opacity:0.6});
+  new google.maps.Marker({position: sp,map: map,icon: marker,opacity:0.6});
+  map.mapTypes.set(MAP_ID, customMapType);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
