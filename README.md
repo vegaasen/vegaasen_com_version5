78f49a8642afdc85b1d11ed351ff82a5df0dfb8c
vegaasen <vegaasen@gmail.com>
2014-11-16 16:39:15 +0100

    Restructured and added some scripts

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 vegaasen_com_version5
 =====================
 
-# Information 
+# Information
 
 Version 5 of my homepage. The plan _this_ time is to make everything as lightweight as possible. There will be no Wordpress integration or anything.
-I will include a link to my blog etc, but thats it - as the blog is not actually a blog - more like a .. um, well place to make notez. 
+I will include a link to my blog etc, but thats it - as the blog is not actually a blog - more like a .. um, well place to make notez.
 ^-- the blog will most likely change as well, as I dont see the point of it :-P
 
 ## Performance
@@ -15,10 +15,16 @@ These are the requirements that I strive to apply:
 * no pointless large js frameworks
 * minified all kinds of stuff
 
+# Generating images
+
+Do the following to generate images:
+
+  
+
 # IDE
 
 * vim
-* sublime 2
+* atom
 
 # Technologies
 
--- resources/js/my.js
+++ resources/js/my.js
@@ -1,17 +1,80 @@
-function destroyLessCache(pathToCss) { // e.g. '/css/' or '/stylesheets/'
- 
+var ele;
+var eleOriginalPositionHeight;
+
+function destroyLessCache(pathToCss) {
   var host = window.location.host;
   var protocol = window.location.protocol;
   var keyPrefix = protocol + '//' + host + pathToCss;
-  
+
   for (var key in window.localStorage) {
     if (key.indexOf(keyPrefix) === 0) {
-      console.log("Removeing cache for " + keyPrefix + "in pathToCss: " + pathToCss);
+      console.log("Removing cache for " + keyPrefix + "in pathToCss: " + pathToCss);
       delete window.localStorage[key];
     }
   }
 }
 
+function onScrollLocation(id, eleNum) {
+  if(ele===undefined) {
+    var element = document.getElementById(id);
+    if(element!==undefined) {
+      ele = element;
+      eleOriginalPositionHeight = ele.offsetHeight;
+    } else {
+      console.log("Element {" + id + "} is undefined.")
+    }
+  }
+}
+
+function onScrollLocationHit(clazz, callback, reset) {
+  if(window.pageYOffset>=eleOriginalPositionHeight) {
+    callback(ele, clazz);
+  } else {
+    reset(ele, clazz);
+  }
+}
+
+function addClickEvent(ele, what) {
+  ele.addEventListener(
+    'click',
+    what,
+    false
+  );
+}
+
 window.onload = function() {
   destroyLessCache("/resources/");
+  onScrollLocation("heading", 0);
+  addClickEvent(document.getElementById("menu-ticker"), function() {
+    var content = document.getElementById("menu-content");
+    var classes = content.classList;
+    if(!classes.contains("display")) {
+      console.log("showing");
+      classes.add("display");
+    }else{
+      console.log("hiding");
+      classes.remove("display");
+    }
+  });
+}
+
+window.onscroll = function() {
+  onScrollLocationHit("switched",
+  function(what, clazz) {
+    classes = what.classList;
+    if(!classes.contains(clazz)) {
+      classes.add(clazz);
+    }
+    what.addEventListener('click',
+    function() {
+      window.scroll(0,0);
+    },
+    false)
+  },
+  function (what, clazz) {
+    classes = what.classList;
+    if(classes.contains(clazz)) {
+      classes.remove(clazz);
+    }
+  });
 }
